4e3acd7109bd3c52417bfcaf467337b776dededc
ladel1 <alatibi@campus-eni.fr>
2021-08-25 16:33:36 +0200

    Correction tp 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +13,33 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class WishController extends AbstractController
 {
+
+    /**
+     * @Route("/add", name="_add")
+     */
+    public function add(): Response
+    {
+        $em=$this->getDoctrine()->getManager();
+        $wish = new Wish();
+        $wish->setTitle("2321321")
+             ->setDescription("32132132")
+             ->setIsPublished(true)
+             ->setAuthor("Sarah")
+             ->setDateCreated(new \DateTime("now"));
+        $em->persist($wish);
+        $em->flush();
+        dd($wish);
+        
+    }
+
     /**
      * @Route("/list", name="_list")
      */
     public function index(): Response
     {
-        return $this->render('wish/index.html.twig');
+        $repo=$this->getDoctrine()->getRepository(Wish::class);
+        $wishes=$repo->findBy(['isPublished'=>true],["dateCreated"=>"DESC"]);        
+        return $this->render('wish/index.html.twig',compact('wishes'));
     }
 
     /**
@@ -24,6 +47,8 @@ class WishController extends AbstractController
      */
     public function detail($id): Response
     {
-        return $this->render('wish/detail.html.twig');
+        $repo=$this->getDoctrine()->getRepository(Wish::class);
+        $wish = $repo->find($id);
+        return $this->render('wish/detail.html.twig',compact('wish'));
     }    
 }
