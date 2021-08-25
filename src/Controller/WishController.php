dae99053bb6ed0afcdd9acdbcc342b3765a336b5
ladel1 <alatibi@campus-eni.fr>
2021-08-25 10:56:47 +0200

    Module 4 + demo module 5

---
+++ src/Controller/WishController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/wish", name="app_wish")
+ */
+class WishController extends AbstractController
+{
+    /**
+     * @Route("/list", name="_list")
+     */
+    public function index(): Response
+    {
+        return $this->render('wish/index.html.twig');
+    }
+
+    /**
+     * @Route("/detail/{id}", name="_detail")
+     */
+    public function detail($id): Response
+    {
+        return $this->render('wish/detail.html.twig');
+    }    
+}
