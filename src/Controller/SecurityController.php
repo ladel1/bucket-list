946cd2972d7a1a5b358c9fd9cf29ff4c84a2b68d
ladel1 <alatibi@campus-eni.fr>
2021-09-01 14:15:53 +0200

    Update css n template with bootstrap

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,10 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
+        if ($this->getUser()) {
+            return $this->redirectToRoute('main');
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
@@ -33,4 +32,5 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
 }
