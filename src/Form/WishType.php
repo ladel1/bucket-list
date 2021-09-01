86c74d12d7db6fdd89d284de5b47e40a47532cb3
ladel1 <alatibi@campus-eni.fr>
2021-09-01 09:53:50 +0200

    Correction tp 8

--- src/Form/WishType.php
+++ src/Form/WishType.php
@@ -7,17 +7,26 @@ use App\Entity\Wish;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class WishType extends AbstractType
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('description')
-            ->add('author')
+            ->add('author',TextType::class,["data"=>$this->security->getUser()->getUserIdentifier()])
             ->add('category',EntityType::class,[ 'class'=>Category::class,
                 'choice_label'=>function($category){
                     return $category->getName();
